040d4b83002519ea45b9bb1f084e5f9293827ccd
arayik0530 <arayik0530@gmail.com>
2024-09-02 06:03:43 +0400

    integration and unit tests implemented

--- src/test/java/com/nobel/rock_paper_scissors/RockPaperScissorsIT.java
+++ src/test/java/com/nobel/rock_paper_scissors/RockPaperScissorsIT.java
@@ -1,7 +1,6 @@
 package com.nobel.rock_paper_scissors;
 
 import com.nobel.rock_paper_scissors.model.*;
-import com.nobel.rock_paper_scissors.repository.PlayerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,7 +26,6 @@ class RockPaperScissorsIT {
         final String password = "password";
         final String testPassword = "testPassword";
 
-        // Step 1: Register a new user
         Map<String, String> registrationRequest = new HashMap<>();
         registrationRequest.put(name, testUser);
         registrationRequest.put(password, testPassword);
@@ -35,7 +33,6 @@ class RockPaperScissorsIT {
         ResponseEntity<Void> registerResponse = restTemplate.postForEntity("/api/auth/signup", registrationRequest, Void.class);
         assertEquals(HttpStatus.OK, registerResponse.getStatusCode());
 
-        // Step 2: Login and retrieve the bearer token
         Map<String, String> loginRequest = new HashMap<>();
         loginRequest.put(name, testUser);
         loginRequest.put(password, testPassword);
@@ -43,21 +40,18 @@ class RockPaperScissorsIT {
         ResponseEntity<LoginResponse> loginResponse = restTemplate.postForEntity("/api/auth/login", loginRequest, LoginResponse.class);
         assertEquals(HttpStatus.OK, loginResponse.getStatusCode());
 
-        // Extract bearer token from login response
         String token = loginResponse.getBody().token();
         assertNotNull(token);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
 
-        // Step 3: Start a new game
         HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
         ResponseEntity<Long> startGameResponse = restTemplate.exchange("/api/game/start", HttpMethod.GET, requestEntity, Long.class);
         assertEquals(HttpStatus.OK, startGameResponse.getStatusCode());
         Long gameId = startGameResponse.getBody();
         assertNotNull(gameId);
 
-        // Step 4: Make a move
         PlayRequest playRequest = new PlayRequest(Move.ROCK, null, gameId);
         HttpEntity<PlayRequest> playRequestEntity = new HttpEntity<>(playRequest, headers);
         ResponseEntity<PlayResponse> playResponse = restTemplate.exchange("/api/game/play", HttpMethod.POST, playRequestEntity, PlayResponse.class);
@@ -67,7 +61,6 @@ class RockPaperScissorsIT {
         assertNotNull(result);
         assertNotNull(result.outcome());
 
-        // Step 5: Finish the game
         ResponseEntity<GameResult> finishGameResponse = restTemplate.exchange("/api/game/" + gameId + "finish", HttpMethod.GET, requestEntity, GameResult.class);
 
         assertEquals(HttpStatus.OK, finishGameResponse.getStatusCode());
@@ -75,6 +68,5 @@ class RockPaperScissorsIT {
         assertNotNull(gameResult);
         assertNotNull(gameResult.playerScore());
         assertNotNull(gameResult.computerScore());
-
     }
 }
